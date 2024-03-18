a8dc4fecf0b989d968dad6d1042dfbf06d5129b5
Armas123 <40261433+Armas123@users.noreply.github.com>
2024-03-18 19:24:16 +0500

    Update calculator.js

--- cypress/integration/calculator.js
+++ cypress/integration/calculator.js
@@ -1,11 +1,15 @@
-describe('Privacy Policy Test', () => {
+describe (  "calculator",()  => {
 
+  it(  "calculator test",()  => {
 
-    it('changing total data stored', () => {
-      // Visit the registration page
-      cy.visit('https://rabata.io');
-      //cy.get('#dataApiStoredInput').as('range').invoke('val', 1).trigger('change');
-     // cy.get('#dataApiStoredInput').trigger('mousemove', {clientX: 1500, clientY: 0});
-      cy.get('#dataApiStoredInput').scrollTo('right');
-    });
-  });
+      cy.visit("https://rabata.io/")
+
+      cy.get('#dataApiStoredInput').invoke('val', 200).trigger('input');
+      cy.wait(2000);
+      // cy.get('#dataApiStoredInput').as('range').invoke('val', 100).trigger('change')
+      cy.get('#dataDownloadInput').invoke('val', 300).trigger('input');
+      cy.wait(1000);
+      cy.get('#rabataMobileApi').should('contain', '$90000 / yr');
+  } )
+
+} )
